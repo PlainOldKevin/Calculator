18eedba1ab47bb951eac4374ab8d94fb303604ac
PlainOldKevin <61211742+PlainOldKevin@users.noreply.github.com>
2022-03-21 15:37:25 -0400

    ask user for two numbers and print sum

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,8 +3,16 @@ import java.util.Scanner;
 public class Calculator {
 	
 	public static void main(String[] args) {
-		Scanner bigsean = new Scanner (System.in);
+		Scanner userInput = new Scanner(System.in);
+		
+		System.out.println("Please enter two integers that will be added together\nType the first number and hit \"enter\", and do the same for the next number");
+		int num1 = userInput.nextInt();
+		int num2 = userInput.nextInt();
+		add(num1, num2);
 		
 	}
 
+	public static void add(int int1,int int2) {
+		System.out.println("Result: " + (int1 + int2));
+	}
 }
