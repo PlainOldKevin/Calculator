d65821b6652af50a814f24aea0dc27d66944aa67
PlainOldKevin <61211742+PlainOldKevin@users.noreply.github.com>
2022-03-21 15:52:02 -0400

    give user choice to add or subtract and perform operation

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,17 +2,33 @@ import java.util.Scanner;
 
 public class Calculator {
 	
+	@SuppressWarnings("unlikely-arg-type")
 	public static void main(String[] args) {
 		Scanner userInput = new Scanner(System.in);
 		
-		System.out.println("Please enter two integers that will be added together\nType the first number and hit \"enter\", and do the same for the next number");
+		System.out.println("Please enter two integers\nType the first number and hit \"enter\", and do the same for the next number\n");
+		
 		int num1 = userInput.nextInt();
 		int num2 = userInput.nextInt();
-		add(num1, num2);
 		
+		System.out.println("Would you like to add or subtract these numbers?\nType \"sub\" for subtract or \"add\" for add");
+		
+		userInput.nextLine();
+		String choice = userInput.nextLine();
+		
+		if (choice.equalsIgnoreCase("add")) {
+			add(num1, num2);
+		}
+		else if(choice.equalsIgnoreCase("sub")) {
+			subtract(num1, num2);
+		}
 	}
 
 	public static void add(int int1,int int2) {
 		System.out.println("Result: " + (int1 + int2));
+	}
+	
+	public static void subtract(int int1, int int2) {
+		System.out.println("Result: " + (int1 - int2));
 	}
 }
