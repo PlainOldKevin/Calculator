e4cae60485b127fe48c179ddfe34469c9438604c
PlainOldKevin <61211742+PlainOldKevin@users.noreply.github.com>
2022-03-21 16:09:26 -0400

    added double and float functionality

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,10 +6,10 @@ public class Calculator {
 	public static void main(String[] args) {
 		Scanner userInput = new Scanner(System.in);
 		
-		System.out.println("Please enter two integers\nType the first number and hit \"enter\", and do the same for the next number\n");
+		System.out.println("Please enter two numbers\nType the first number and hit \"enter\", and do the same for the next number\n");
 		
-		int num1 = userInput.nextInt();
-		int num2 = userInput.nextInt();
+		double num1 = userInput.nextDouble();
+		double num2 = userInput.nextDouble();
 		
 		System.out.println("Would you like to add, subtract, multiply, or divide these numbers?");
 		System.out.println("Type \"sub\" to subtract, \"add\" to add, \"mult\" to multiply, or \"div\" to divide");
@@ -31,19 +31,19 @@ public class Calculator {
 		}
 	}
 
-	public static void add(int int1,int int2) {
+	public static void add(double int1, double int2) {
 		System.out.println("Result: " + (int1 + int2));
 	}
 	
-	public static void subtract(int int1, int int2) {
+	public static void subtract(double int1, double int2) {
 		System.out.println("Result: " + (int1 - int2));
 	}
 	
-	public static void multiply(int int1, int int2) {
+	public static void multiply(double int1, double int2) {
 		System.out.println("Result: " + (int1 * int2));
 	}
 	
-	public static void divide(int int1, int int2) {
+	public static void divide(double int1, double int2) {
 		System.out.println("Result: " + (int1 / int2));
 	}
 }
